2e4159269c20e7234a93ece0aed1bebd9088f3bd
Gyanendra Mishra <anomaly.the@gmail.com>
2023-06-05 12:01:13 +0530

    work in progress

--- go-utils/main.go
+++ go-utils/main.go
@@ -2,15 +2,56 @@ package main
 
 import (
 	"fmt"
+	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/staking"
+	"os"
+	"strconv"
 )
 
 const (
-	stakingNodeKeyPath  = "/tmp/data/staking/staker.key"
-	stakingNodeCertPath = "/tmp/data/staking/staker.cert"
+	stakingNodeKeyPath  = "/tmp/data/staking/node-%d/staker.key"
+	stakingNodeCertPath = "/tmp/data/staking/node-%d/staker.cert"
+	numNodeArgIndex     = 1
+	networkIdIndex      = 2
+	nonZeroExitCode     = 1
 )
 
 func main() {
-	err := staking.InitNodeStakingKeyPair(stakingNodeKeyPath, stakingNodeCertPath)
-	fmt.Println(err)
+	if len(os.Args) < networkIdIndex {
+		fmt.Printf("Need at least 2 more args apart from program name got '%v' total", len(os.Args))
+		os.Exit(nonZeroExitCode)
+	}
+
+	numNodesArg := os.Args[numNodeArgIndex]
+	numNodes, err := strconv.Atoi(numNodesArg)
+	if err != nil {
+		fmt.Printf("An error occurred while converting numNodes arg to integer: %v\n", err)
+		os.Exit(nonZeroExitCode)
+	}
+
+	networkId := os.Args[networkIdIndex]
+
+	fmt.Printf("Have a total of '%v' nodes to generate and network id '%v'\n", numNodes, networkId)
+	var nodeIds []ids.NodeID
+
+	for index := 0; index < numNodes; index++ {
+		keyPath := fmt.Sprintf(stakingNodeKeyPath, index)
+		certPath := fmt.Sprintf(stakingNodeCertPath, index)
+		err = staking.InitNodeStakingKeyPair(keyPath, certPath)
+		if err != nil {
+			fmt.Printf("An error occurred while generating keys for node %v: %v\n", index+1, err)
+			os.Exit(nonZeroExitCode)
+		}
+		fmt.Printf("Gnerated key and cert for node '%v' at '%v', '%v\n", index+1, keyPath, certPath)
+		cert, err := staking.LoadTLSCertFromFiles(keyPath, certPath)
+		if err != nil {
+			fmt.Printf("an error occurred while loading cert pair for node '%v': %v\n", index+1, err)
+			os.Exit(nonZeroExitCode)
+		}
+		nodeId := ids.NodeIDFromCert(cert.Leaf)
+		fmt.Printf("node '%v' has node id '%v'\n", nodeId, nodeId)
+		nodeIds = append(nodeIds, nodeId)
+	}
+
+	fmt.Println(nodeIds)
 }
