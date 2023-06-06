1c7c77f413780d40c98fd7366c6661b365ce8f98
Gyanendra Mishra <anomaly.the@gmail.com>
2023-06-06 16:01:48 +0530

    create new vwallet

--- wallet/main.go
+++ wallet/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/ava-labs/avalanchego/genesis"
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/constants"
+	"github.com/ava-labs/avalanchego/vms/avm"
+	"github.com/ava-labs/avalanchego/vms/platformvm"
+	"github.com/ava-labs/avalanchego/vms/platformvm/txs"
+	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
+	"github.com/ava-labs/avalanchego/wallet/chain/p"
+	"github.com/ava-labs/avalanchego/wallet/chain/x"
+	"github.com/ava-labs/avalanchego/wallet/subnet/primary"
+)
+
+type wallet struct {
+	addr     ids.ShortID
+	pWallet  p.Wallet
+	pBackend p.Backend
+	pBuilder p.Builder
+	pSigner  p.Signer
+	xWallet  x.Wallet
+}
 
 func main() {
 	fmt.Println("hello")
+}
+
+func newWallet(uri string) (*wallet, error) {
+	ctx := context.Background()
+	kc := secp256k1fx.NewKeychain(genesis.EWOQKey)
+	pCTX, xCTX, utxos, err := primary.FetchState(ctx, uri, kc.Addresses())
+	if err != nil {
+		return nil, err
+	}
+	// platform client
+	pClient := platformvm.NewClient(uri)
+	// TODO verify previous transactions - can be empty
+	pTXs := make(map[ids.ID]*txs.Tx)
+	// TODO perhaps this chain ID should be 43112 and not empty
+	pUTXOs := primary.NewChainUTXOs(constants.PlatformChainID, utxos)
+	xChainID := xCTX.BlockchainID()
+	xUTXOs := primary.NewChainUTXOs(xChainID, utxos)
+	var w wallet
+	w.addr = genesis.EWOQKey.PublicKey().Address()
+	w.pBackend = p.NewBackend(pCTX, pUTXOs, pTXs)
+	w.pBuilder = p.NewBuilder(kc.Addresses(), w.pBackend)
+	w.pSigner = p.NewSigner(kc, w.pBackend)
+	w.pWallet = p.NewWallet(w.pBuilder, w.pSigner, pClient, w.pBackend)
+
+	xBackend := x.NewBackend(xCTX, xChainID, xUTXOs)
+	xBuilder := x.NewBuilder(kc.Addresses(), xBackend)
+	xSigner := x.NewSigner(kc, xBackend)
+	xClient := avm.NewClient(uri, "X")
+	w.xWallet = x.NewWallet(xBuilder, xSigner, xClient, xBackend)
+	return &w, nil
+
 }
