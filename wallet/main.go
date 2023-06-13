b887d708b6cb4bce594cd47d174d2c5e8ee4e86a
Gyanendra Mishra <anomaly.the@gmail.com>
2023-06-13 16:00:45 +0100

    this should owrk

--- wallet/main.go
+++ wallet/main.go
@@ -36,7 +36,7 @@ const (
 	nodeIdPathFormat       = "/tmp/data/node-%d/node_id.txt"
 
 	// validate from a minute after now
-	startTimeDelayFromNow = 1 * time.Minute
+	startTimeDelayFromNow = 2 * time.Minute
 	// validate for 14 days
 	endTimeFromStartTime = 28 * 24 * time.Hour
 	// random stake weight of 200
